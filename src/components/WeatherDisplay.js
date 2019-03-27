f44f6d4f6c312b5259fcd08a336eb24a5f883029
unknown <chris.a.desilva@gmail.com>
2019-03-27 14:25:27 -0500

    Update background, form, and report styles

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -4,15 +4,14 @@ import '../App.css'
 
 const WeatherDisplay = props => (
   <div>
-    <h1 style={{textAlign: "center", marginTop: "2.5rem"}}>Weather Report</h1>
+    <h1 style={{textAlign: "center", paddingTop: "12rem"}}>Weather Report</h1>
     <Grid divided stackable columns={2} style={{marginTop: "2.5rem"}}>
       <Grid.Column className="column">
         <Form error onSubmit={props.getWeather} >
           <Form.Field>
-            <label>Zip Code</label>
             <Form.Input type ="text" name="zipCode" placeholder="Enter your zip code"/>
           </Form.Field>
-          <Button primary type="submit">Get Weather</Button>
+          <Button secondary type="submit">Get Weather</Button>
           {props.error &&
             <Message 
               error
@@ -22,13 +21,15 @@ const WeatherDisplay = props => (
           }
         </Form>
       </Grid.Column>
-      <Grid.Column className="column">
-        {props.zipCode && <p>Weather in the <strong>{props.name}</strong> area</p>}
-        {props.temp && <p>Temperature: {props.temp}°F</p>}
-        {props.humidity && <p>Humidity: {props.humidity}%</p>}
-        {props.conditions && <p>Conditions: {props.conditions}</p>}
-        {props.icon && <Image src={`http://openweathermap.org/img/w/${props.icon}.png`} alt="Weather details icon"/>}
-      </Grid.Column>
+      {props.zipCode &&
+        <Grid.Column className="report">
+          {props.zipCode && <p>Weather in the <strong>{props.name}</strong> area</p>}
+          {props.temp && <p>Temperature: {props.temp}°F</p>}
+          {props.humidity && <p>Humidity: {props.humidity}%</p>}
+          {props.conditions && <p>Conditions: {props.conditions}</p>}
+          {props.icon && <Image src={`http://openweathermap.org/img/w/${props.icon}.png`} alt="Weather details icon"/>}
+        </Grid.Column>
+      }
     </Grid>
   </div>
 
